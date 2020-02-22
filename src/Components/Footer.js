120acbe7e8a074ce9f939d0c2f54da9e9955143e
Esen <esenkalmanbetov@gmail.com>
2020-02-23 00:46:47 +0600

    copied index.html and modified

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,11 +2,176 @@ import React from "react";
 
 import "./Footer.scss";
 
+import FooterLogo from "../assets/img/footer_logo.png";
+
+import Instagram1 from "../assets/img/instagram/1.png";
+import Instagram2 from "../assets/img/instagram/2.png";
+import Instagram3 from "../assets/img/instagram/3.png";
+import Instagram4 from "../assets/img/instagram/4.png";
+import Instagram5 from "../assets/img/instagram/5.png";
+import Instagram6 from "../assets/img/instagram/6.png";
+
 class Footer extends React.Component {
   render() {
     return (
-      <footer>
-        <h2>This is footer</h2>
+      <footer class="footer">
+        <div class="footer_top">
+          <div class="container">
+            <div class="row">
+              <div class="col-xl-4 col-md-6 col-lg-4 ">
+                <div class="footer_widget">
+                  <div class="footer_logo">
+                    <a href="#">
+                      <img src={FooterLogo} alt="" />
+                    </a>
+                  </div>
+                  <p>
+                    5th flora, 700/D kings road, green <br /> lane New York-1782{" "}
+                    <br />
+                    <a href="#">+10 367 826 2567</a> <br />
+                    <a href="#">contact@carpenter.com</a>
+                  </p>
+                  <div class="socail_links">
+                    <ul>
+                      <li>
+                        <a href="#">
+                          <i class="ti-facebook"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="ti-twitter-alt"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fa fa-instagram"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fa fa-pinterest"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fa fa-youtube-play"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-2 col-md-6 col-lg-2">
+                <div class="footer_widget">
+                  <h3 class="footer_title">Company</h3>
+                  <ul class="links">
+                    <li>
+                      <a href="#">Pricing</a>
+                    </li>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                    <li>
+                      <a href="#"> Gallery</a>
+                    </li>
+                    <li>
+                      <a href="#"> Contact</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-xl-3 col-md-6 col-lg-3">
+                <div class="footer_widget">
+                  <h3 class="footer_title">Popular destination</h3>
+                  <ul class="links double_links">
+                    <li>
+                      <a href="#">Indonesia</a>
+                    </li>
+                    <li>
+                      <a href="#">America</a>
+                    </li>
+                    <li>
+                      <a href="#">India</a>
+                    </li>
+                    <li>
+                      <a href="#">Switzerland</a>
+                    </li>
+                    <li>
+                      <a href="#">Italy</a>
+                    </li>
+                    <li>
+                      <a href="#">Canada</a>
+                    </li>
+                    <li>
+                      <a href="#">Franch</a>
+                    </li>
+                    <li>
+                      <a href="#">England</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-xl-3 col-md-6 col-lg-3">
+                <div class="footer_widget">
+                  <h3 class="footer_title">Instagram</h3>
+                  <div class="instagram_feed">
+                    <div class="single_insta">
+                      <a href="#">
+                        <img src={Instagram1} alt="" />
+                      </a>
+                    </div>
+                    <div class="single_insta">
+                      <a href="#">
+                        <img src={Instagram2} alt="" />
+                      </a>
+                    </div>
+                    <div class="single_insta">
+                      <a href="#">
+                        <img src={Instagram3} alt="" />
+                      </a>
+                    </div>
+                    <div class="single_insta">
+                      <a href="#">
+                        <img src={Instagram4} alt="" />
+                      </a>
+                    </div>
+                    <div class="single_insta">
+                      <a href="#">
+                        <img src={Instagram5} alt="" />
+                      </a>
+                    </div>
+                    <div class="single_insta">
+                      <a href="#">
+                        <img src={Instagram6} alt="" />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="copy-right_text">
+          <div class="container">
+            <div class="footer_border"></div>
+            <div class="row">
+              <div class="col-xl-12">
+                <p class="copy_right text-center">
+                  {/* <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> */}
+                  Copyright &copy;
+                  <script>document.write(new Date().getFullYear());</script> All
+                  rights reserved | This template is made with{" "}
+                  <i class="fa fa-heart-o" aria-hidden="true"></i> by{" "}
+                  <a href="https://colorlib.com" target="_blank">
+                    Colorlib
+                  </a>
+                  {/* <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> */}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </footer>
     );
   }
