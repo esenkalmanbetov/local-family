57810b35ed3e4d1c8839a721f022168d66906e56
Esen <esenkalmanbetov@gmail.com>
2020-04-24 05:50:06 +0600

    config changed

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,17 @@
 export default {
   apiUrl:
-    process.env.NODE_ENV === "production"
-      ? "https://local-family-backend.herokuapp.com"
-      : "http://localhost:8080",
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8080"
+      : "https://local-family-backend.herokuapp.com",
 };
 
 // development: http://localhost:8080
 //  production: https://local-family-backend.herokuapp.com
+
+// if (process.env.NODE_ENV === "development") {
+//   url = "https://google.com";
+// }
+
+// if (process.env.NODE_ENV === "production") {
+//   url = "https://stackoverflow.com/";
+// }
