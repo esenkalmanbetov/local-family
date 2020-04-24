85100ce17ddc8d486102bf089e3fad96411e31f5
Esen <esenkalmanbetov@gmail.com>
2020-04-24 06:12:29 +0600

    config changed

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 export default {
   apiUrl:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:8080"
-      : "https://local-family-backend.herokuapp.com",
+    process.env.NODE_ENV === "production"
+      ? "https://local-family-backend.herokuapp.com"
+      : "http://localhost:8080",
 };
 
 // development: http://localhost:8080
