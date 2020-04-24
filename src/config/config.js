22f995232a7cad3073e54bae990e6fd04332220f
Esen <esenkalmanbetov@gmail.com>
2020-04-24 06:25:30 +0600

    fadsfsa

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,9 @@
 export default {
   apiUrl:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:8080"
-      : "https://local-family-backend.herokuapp.com",
+      ? "https://local-family-backend.herokuapp.com"
+      : "http://localhost:8080",
 };
 
 // development: http://localhost:8080
 //  production: https://local-family-backend.herokuapp.com
-
-// if (process.env.NODE_ENV === "development") {
-//   url = "https://google.com";
-// }
-
-// if (process.env.NODE_ENV === "production") {
-//   url = "https://stackoverflow.com/";
-// }
